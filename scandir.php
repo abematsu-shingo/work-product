48e15668d84cf6388c9270e468ea3598aef65d6c
abematsu <abematsu.shingo@gmail.com>
2025-06-10 17:35:47 +0900

    ディレクトリの存在チェックの条件を追加

--- scandir.php
+++ scandir.php
@@ -13,26 +13,42 @@ $base_url = 'https://lgqqi65169.rakkoserver.net/work-product/';
 // ファイルリストを格納する配列
 $file_list = [];
 
-// 
-$files = array_diff(scandir($directory), ['.', '..']);
-// var_dump($files);
+// ディレクトリが存在しているかチェック
+if(is_dir($directory)){
+    // ディレクトリが存在していた場合の処理
+    
+    // 取得したファイルから、'.' と '..' を削除
+    $files = array_diff(scandir($directory), ['.', '..']);
+    // var_dump($files);
 
-// 取得したファイルをURLに変換するためforeachで展開。
-foreach($files as $file) {
-    // var_dump($file);
+    // 取得したファイルをURLに変換するためforeachで展開。
+    foreach($files as $file) {
+        // var_dump($file);
 
-    // ファイル名に使用された特殊文字をURL用にエンコード
-    $file_url = $base_url . urlencode($file);
-    // var_dump($file_url);
+        // ファイル名に使用された特殊文字をURL用にエンコード
+        $file_url = $base_url . urlencode($file);
+        // var_dump($file_url);
 
-    // 取得したファイルを配列に格納
-    $file_list[] = [
-        // 連想配列（キー）を指定
-        'name' => $file,
-        'url' => $file_url
-    ];
+        // 取得したファイルを配列に格納
+        $file_list[] = [
+            // 連想配列（キー）を指定
+            'name' => $file,
+            'url' => $file_url
+        ];
+    }
+    // ディレクトリが存在していた場合は、$file_listの配列を返す
+    echo json_encode([
+        'result' => true,
+        'files' => $file_list
+    ]);
+} else {
+    // ディレクトリが存在しない場合のエラーレスポンス
+    echo json_encode([
+        'result' => false,
+        'message' => '指定されたディレクトリが見つかりません。'
+    ]);
 }
 // var_dump($file_url);
-var_dump($file_list);
+// var_dump($file_list);
 
 ?>
