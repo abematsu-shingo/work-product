7feadb7cf23c272879a4674e514fa747a246ff7a
abematsu <abematsu.shingo@gmail.com>
2025-06-10 17:59:56 +0900

    URL生成の処理をまとめて、コメントアウトを調整

--- scandir.php
+++ scandir.php
@@ -5,50 +5,45 @@ header('Access-Control-Allow-Origin: https://lgqqi65169.rakkoserver.net');
 // json形式でデータ送信
 header('Content-Type: application/json');
 
-// echo $_SERVER['DOCUMENT_ROOT'];
+// ディレクトリの絶対パス指定
 $directory = $_SERVER['DOCUMENT_ROOT'] . '/work-product/';
-// WEB上のルートURL。「$base_url」+「ファイル名」でURLの生成。
-$base_url = 'https://lgqqi65169.rakkoserver.net/work-product/';
 
 // ファイルリストを格納する配列
 $file_list = [];
 
 // ディレクトリが存在しているかチェック
 if(is_dir($directory)){
-    // ディレクトリが存在していた場合の処理
     
     // 取得したファイルから、'.' と '..' を削除
     $files = array_diff(scandir($directory), ['.', '..']);
-    // var_dump($files);
-
+    
     // 取得したファイルをURLに変換するためforeachで展開。
     foreach($files as $file) {
-        // var_dump($file);
-
-        // ファイル名に使用された特殊文字をURL用にエンコード
+        
+        // WEB上のルートURL + ファイル名 でURL生成
+        $base_url = 'https://lgqqi65169.rakkoserver.net/work-product/';
         $file_url = $base_url . urlencode($file);
-        // var_dump($file_url);
 
         // 取得したファイルを配列に格納
         $file_list[] = [
-            // 連想配列（キー）を指定
             'name' => $file,
             'url' => $file_url
         ];
     }
+
     // ディレクトリが存在していた場合は、$file_listの配列を返す
     echo json_encode([
         'result' => true,
         'files' => $file_list
     ]);
+
 } else {
+    
     // ディレクトリが存在しない場合のエラーレスポンス
     echo json_encode([
         'result' => false,
         'message' => '指定されたディレクトリが見つかりません。'
     ]);
 }
-// var_dump($file_url);
-// var_dump($file_list);
 
 ?>
