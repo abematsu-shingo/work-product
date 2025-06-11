9a0dff6c56c68d6ca2e10dcff76ec5ad3623bc09
abematsu <abematsu.shingo@gmail.com>
2025-06-11 13:08:37 +0900

    APIからデータを取得する処理を追加し、リスト出力ボタンを実装

--- vue-work-product/src/App.vue
+++ vue-work-product/src/App.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+
+// 取得したデータの中身を格納
+const files = ref([])
+
+// データ取得する処理
+const fetchFiles = async (Promise) => {
+  const getData = await axios.get('/api/scandir.php')
+  // console.log(getData.data.files)
+  files.value = getData.data.files
+}
+</script>
 <template>
   <h1>Work Product</h1>
+
+  <!-- ボタンクリックでファイル取得 -->
+  <button @click="fetchFiles">getFile</button>
+
+  <!-- 取得したファイルをリスト出力 -->
+  <ul>
+    <li v-for="file in files">
+      <a :href="file.url" target="_blank">{{ file.name }}</a>
+    </li>
+  </ul>
 </template>
 <style scoped></style>
