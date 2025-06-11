7441c10ab97d890a35c3f406a51cae522199df79
abematsu <abematsu.shingo@gmail.com>
2025-06-11 13:55:23 +0900

    API取得処理の復習

--- vue-work-product/src/App.vue
+++ vue-work-product/src/App.vue
@@ -2,26 +2,27 @@
 import axios from 'axios'
 import { ref } from 'vue'
 
-// 取得したデータの中身を格納
+// 取得したデータを格納
 const files = ref([])
 
-// データ取得する処理
-const fetchFiles = async () => {
-  const getData = await axios.get('/api/scandir.php')
-  // console.log(getData.data.files)
-  files.value = getData.data.files
+// APIデータを取得する処理
+const getApi = async () => {
+  const api = await axios.get('/api/scandir.php')
+
+  // 取得したデータをfilesへ格納
+  // scrpit内でrefの中身を操作する場合は、明示的にvalueを指定
+  files.value = api.data.files
 }
 </script>
 <template>
   <h1>Work Product</h1>
+  <!-- apiデータを取得するボタン -->
+  <button @click="getApi">Get Api</button>
 
-  <!-- ボタンクリックでファイル取得 -->
-  <button @click="fetchFiles">getFile</button>
-
-  <!-- 取得したファイルをリスト出力 -->
+  <!-- 取得したデータをリスト出力 -->
   <ul>
     <li v-for="file in files">
-      <a :href="file.url" target="_blank">{{ file.name }}</a>
+      <a :href="file.url">{{ file.name }}</a>
     </li>
   </ul>
 </template>
