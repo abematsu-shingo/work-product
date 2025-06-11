b2bddf47e72ec0d50216747149cb86edc92ae1b2
abematsu <abematsu.shingo@gmail.com>
2025-06-11 14:38:27 +0900

    ディレクトリが存在しなかった場合に表示されるメッセージを追加

--- vue-work-product/src/App.vue
+++ vue-work-product/src/App.vue
@@ -4,14 +4,21 @@ import { ref } from 'vue'
 
 // 取得したデータを格納
 const files = ref([])
+const errerMessage = ref('')
 
 // APIデータを取得する処理
 const getApi = async () => {
   const api = await axios.get('/api/scandir.php')
+  console.log(api.data)
 
-  // 取得したデータをfilesへ格納
-  // scrpit内でrefの中身を操作する場合は、明示的にvalueを指定
-  files.value = api.data.files
+  if (api.data.result) {
+    // apiデータのresultがtrueだった場合、取得したデータをfilesへ格納
+    // scrpit内でrefの中身を操作する場合は、明示的にvalueを指定
+    files.value = api.data.files
+  } else {
+    // apiデータのresultがfalseだった場合、messageをerrerMessageに代入
+    errerMessage.value = api.data.message // 'message' => '指定されたディレクトリが見つかりません。'
+  }
 }
 </script>
 <template>
@@ -25,5 +32,6 @@ const getApi = async () => {
       <a :href="file.url" target="_blank" rel="noopener noreferrer">{{ file.name }}</a>
     </li>
   </ul>
+  <p v-if="errerMessage">{{ errerMessage }}</p>
 </template>
 <style scoped></style>
